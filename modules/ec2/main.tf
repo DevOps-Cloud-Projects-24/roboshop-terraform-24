34d21c281baa4e60b904bec8c70092ddd0085133
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-08-22 23:49:05 -0500

    Roboshop terraform SGs added for web-servers for frontend

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -30,3 +30,15 @@ resource "aws_security_group" "main" {
   }
 }
 
+resource "aws_launch_template" "main" {
+  name = "${var.name}-${var.env}-lt"
+  image_id = data.aws_ami.rhel9.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tags = {
+    Name = "${var.name}-${var.env}-sg"
+  }
+
+}
+
